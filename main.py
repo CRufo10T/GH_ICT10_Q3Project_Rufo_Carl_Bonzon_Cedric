6dd81bc4fcb08f1f91f698429c7c9dcd89fe3839
CRufo10T <rufo.cg@obmontessori.edu.ph>
2026-02-23 08:04:09 +0800

    Refactor player display logic in Play1 function

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ def Play1(e):
 
 
     Players = {
-    '1': 'Jalaine Abdullah',
-    '2' : 'Leona Abeleda',
-    '3' : 'Caleb Arias', 
-    '4' : 'Renzo Arce',
-    '5' : ' Martina Cajucom',
-    '6' : 'Sean Cotioco',
-     '7': 'Sang Heon Choi',
-    '8' : 'Phoebe Catimbang',
-    '9' : 'Alejandro Enriquez', 
-    '10' : 'Skyler Escobar',
-    '11' : 'Khloe Espina',
-    '12' : 'Prince Akishino Gano',
-     '13': 'Calvin Garcia',
-    '14' : 'Simrandip Kaur',
-    '15' : 'Sebastian Chilli Ong', 
-    '16' : 'Carl Joseph Rufo',
-    '17' : ' Miquel Joaquin Sanchez',
-    '18' : 'Ramon Santos',
-     '19': 'Allen Daradal',
-    '20' : 'Beatrice Vilale',
-    '21' : 'Deryck Tan', 
-    '22' : 'Harmony Gail Yao',
-    '23' : 'Ivy Zosa',
-    '24' : 'Cedric Bonzon',
+     'Jalaine Abdullah',
+     'Leona Abeleda',
+     'Caleb Arias', 
+    'Renzo Arce',
+     ' Martina Cajucom',
+     'Sean Cotioco',
+      'Sang Heon Choi',
+     'Phoebe Catimbang',
+     'Alejandro Enriquez', 
+     'Skyler Escobar',
+     'Khloe Espina',
+     'Prince Akishino Gano',
+      'Calvin Garcia',
+     'Simrandip Kaur',
+     'Sebastian Chilli Ong', 
+     'Carl Joseph Rufo',
+     ' Miquel Joaquin Sanchez',
+     'Ramon Santos',
+      'Allen Daradal',
+     'Beatrice Vilale',
+     'Deryck Tan', 
+     'Harmony Gail Yao',
+       'Ivy Zosa',
+     'Cedric Bonzon',
     
     }
     document.getElementById("output").innerHTML = ""
@@ -36,27 +36,5 @@ def Play1(e):
 
     
 
-    display(f'{Players['1']}', target='output')
-    display(f'{Players['2']}', target='output')
-    display(f'{Players['3']}', target='output')
-    display(f'{Players['4']}', target='output')
-    display(f'{Players['5']}',target='output')
-    display(f'{Players['6']}', target='output')
-    display(f'{Players['7']}', target='output')
-    display(f'{Players['8']}', target='output')
-    display(f'{Players['9']}', target='output')
-    display(f'{Players['10']}', target='output')
-    display(f'{Players['11']}',target='output')
-    display(f'{Players['12']}', target='output')
-    display(f'{Players['13']}', target='output')
-    display(f'{Players['14']}', target='output')
-    display(f'{Players['15']}', target='output')
-    display(f'{Players['16']}', target='output')
-    display(f'{Players['17']}',target='output')
-    display(f'{Players['18']}', target='output')
-    display(f'{Players['19']}', target='output')
-    display(f'{Players['20']}', target='output')
-    display(f'{Players['21']}', target='output')
-    display(f'{Players['22']}', target='output')
-    display(f'{Players['23']}',target='output')
-    display(f'{Players['24']}', target='output')
+    for num in Players:
+        display (num, target='output')
